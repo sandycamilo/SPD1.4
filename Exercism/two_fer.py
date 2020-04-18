b0eb0910874237afcb2c51e80d5829196744f71a
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-17 21:39:44 -0700

    pseudo & defaults def

--- Exercism/two_fer.py
+++ Exercism/two_fer.py
@@ -18,12 +18,21 @@
  # Check your work - test your code
 
 
- # return text with the message 'one for x, one for you' where x is name  
- # but if there is no name, return one for you and one for me
+# understand:
+# return text with the message 'one for x, one for you' where x is name  
+# but if there is no name, return one for you and one for me
 
+# pattern:
+# return sentence based on input
+
+# pseudocode:
+# define function with input as argument 
+# if the input is None then return the sentence 'one for you, one for me'
+# else if the input is entered, return the sentence 'one for you, one for me' where you is replaced with the word entered 
 
 def two_fer(name={}):
     if name is two_fer.__defaults__[0]:
+        # __defaults__ is  a tuple containing default argument values for those arguments that have defaults, or None if no arguments have a default value
         sentence = "One for you, one for me."
         return sentence
     else:
