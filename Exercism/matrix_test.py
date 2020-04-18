b1662cddac90b8c9845e7b4f4efe607eac074931
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-17 22:42:20 -0700

    test_rows output

---
+++ Exercism/matrix_test.py
@@ -0,0 +1,13 @@
+from matrix import Matrix
+import unittest
+
+class MatrixTest(unittest.TestCase):
+
+    def test_rows(self):
+        assert Matrix.row('9 8 7\n5 3 2\n6 6 7') == '987532667'
+
+    def test_columns(self):
+        assert Matrix.column('9 8 7\n5 3 2\n6 6 7') == '956836727'
+
+if __name__ == '__main__':
+    unittest.main()
